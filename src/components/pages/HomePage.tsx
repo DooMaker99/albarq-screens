1992eee7aeb4d18dd00fe34eb2badffc65e9b4eb
Wix Remote Machine <support@wix.com>
2025-12-23 08:57:05 +0000

    Adjusting image/container layering.

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -153,7 +153,7 @@ export default function HomePage() {
                 className="absolute inset-4 lg:inset-y-8 lg:left-0 lg:right-[-50vw] bg-gradient-to-br from-[#F5F7FF] to-[#E0E7FF] overflow-hidden shadow-2xl z-5"
               >
                 {/* Abstract 3D Wave Image Placeholder */}
-                <div className="absolute inset-0 w-full h-full">
+                <div className="absolute inset-0 w-full h-full z-0">
                    <Image 
                     src="https://static.wixstatic.com/media/fe743e_b99d2f91ffff464ca8faab305036a458~mv2.png?originWidth=1600&originHeight=896"
                     alt="Abstract digital wave representing screen technology"
